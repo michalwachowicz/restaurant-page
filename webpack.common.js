57ba66000a4b0eac20c6cb0cb840fbc0aa0f1b9f
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-10 13:13:21 +0200

    Install and configure sass loader in webpack

--- webpack.common.js
+++ webpack.common.js
@@ -8,6 +8,14 @@ module.exports = {
       title: "Sushi Bar | Home",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
