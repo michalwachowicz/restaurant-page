c3218806e01a857d4891be668471f7973bf28711
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-10 13:21:02 +0200

    Create index.js file and test functionality

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const content = document.createElement("div");
+content.id = "content";
+
+const h1 = document.createElement("h1");
+h1.textContent = "Hello World!";
+
+content.appendChild(h1);
+document.body.appendChild(content);
+
+alert("Everything works!");
