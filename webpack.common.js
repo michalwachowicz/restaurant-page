6e652745d013cd47a3c883ce42e11046cc96d53f
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-11 13:52:45 +0200

    Minify css files only in production mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,17 +1,11 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       title: "Sushi Bar | Home",
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
     }),
   ],
   module: {
@@ -26,23 +20,11 @@ module.exports = {
           },
         },
       },
-      {
-        test: /.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
     ],
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
   },
 };
