1531de69a40d60c59074bb5061a91817d58ee142
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-13 01:08:11 +0200

    Create and style main page container

---
+++ src/js/components/container.js
@@ -0,0 +1,6 @@
+export default function createContainer() {
+  const container = document.createElement("div");
+  container.classList.add("container");
+
+  return container;
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,4 @@
 import "../scss/style.scss";
+import createContainer from "./components/container";
 
-const content = document.createElement("div");
-content.id = "content";
-
-const h1 = document.createElement("h1");
-h1.textContent = "Hello World!";
-
-content.appendChild(h1);
-document.body.appendChild(content);
-
-alert("Everything works!");
+document.body.appendChild(createContainer());
