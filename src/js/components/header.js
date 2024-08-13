f7ce7dc0677e2f14b66566774c543fe1aa84d348
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-14 00:29:08 +0200

    Add icon buttons to the header

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,5 +1,10 @@
+import createButton from "./button";
+import createImage from "./image";
 import createLogo from "./logo";
 import createNavigation from "./navigation";
+
+import BasketIcon from "../../assets/icons/basket-dark.svg";
+import MenuIcon from "../../assets/icons/menu.svg";
 
 export default function createHeader() {
   const header = document.createElement("header");
@@ -7,7 +12,12 @@ export default function createHeader() {
 
   header.append(
     createLogo("dark", "header-logo"),
-    createNavigation("header-nav")
+    createNavigation("header-nav"),
+    createButton(
+      createImage(BasketIcon, "Basket/cart icon", 100, 100),
+      "btn-header"
+    ),
+    createButton(createImage(MenuIcon, "Menu icon", 100, 100), "btn-header")
   );
 
   return header;
