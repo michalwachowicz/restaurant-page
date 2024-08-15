6385cfab4b3c97b2c3e8a697d47f19321d1108f2
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-15 19:52:00 +0200

    Create sushi menu samples section structure

--- src/js/layout/container.js
+++ src/js/layout/container.js
@@ -1,11 +1,12 @@
 import createHeader from "./header";
 import createMain from "./main";
+import createMenuSamplesSection from "./samples";
 
 export default function createContainer() {
   const container = document.createElement("div");
   container.classList.add("container");
 
-  container.append(createHeader(), createMain());
+  container.append(createHeader(), createMain(), createMenuSamplesSection());
 
   return container;
 }
---
+++ src/js/layout/samples.js
@@ -0,0 +1,80 @@
+import createButton from "../components/button";
+import createAddToCartBtn from "../components/add-to-cart-btn";
+import createCard from "../components/card";
+import createSection from "./section";
+
+import SalmonUramaki from "../../assets/img/sushi-1.png";
+import TunaUramaki from "../../assets/img/sushi-2.png";
+import SalmonNigiri from "../../assets/img/sushi-3.png";
+import createImage from "../components/image";
+
+const labels = [
+  { name: "All Sushi", active: true },
+  { name: "Sashimi", active: false },
+  { name: "Nigiri", active: false },
+  { name: "Uramaki", active: false },
+  { name: "Temaki", active: false },
+  { name: "Futomaki", active: false },
+];
+
+const SUSHI_DESCRIPTION =
+  "Delicately crafted with fresh ingredients, perfect for every sushi lover.";
+
+const sushiSamples = [
+  {
+    name: "Salmon Uramaki",
+    description: SUSHI_DESCRIPTION,
+    src: SalmonUramaki,
+    alt: "A sushi piece of salmon uramaki",
+  },
+  {
+    name: "Tuna Uramaki",
+    description: SUSHI_DESCRIPTION,
+    src: TunaUramaki,
+    alt: "A sushi piece of tuna uramaki",
+  },
+  {
+    name: "Salmon Nigiri",
+    description: SUSHI_DESCRIPTION,
+    src: SalmonNigiri,
+    alt: "A sushi piece of salmon nigiri",
+  },
+];
+
+export default function createMenuSamplesSection() {
+  const section = createSection(
+    "Savor the Art of Sushi Perfection",
+    "Embark on a culinary journey where every bite is a masterpiece. For <strong>JUST $30</strong>, enjoy unlimited sushi, expertly crafted with the freshest ingredients to delight your senses."
+  );
+  section.classList.add("menu-section");
+
+  const labelContainer = document.createElement("div");
+  labelContainer.classList = "menu-section-label-container";
+
+  labels.forEach((label) => {
+    const btn = createButton(
+      label.name,
+      `btn btn-label ${label.active ? "active" : ""}`
+    );
+    labelContainer.appendChild(btn);
+  });
+
+  const cardContainer = document.createElement("div");
+  cardContainer.classList = "menu-section-card-container";
+
+  sushiSamples.forEach((sushi) =>
+    cardContainer.appendChild(
+      createCard(
+        "menu-section-card",
+        sushi.name,
+        sushi.description,
+        createImage(sushi.src, sushi.alt, 150, 150),
+        createAddToCartBtn()
+      )
+    )
+  );
+
+  section.append(labelContainer, cardContainer);
+
+  return section;
+}
