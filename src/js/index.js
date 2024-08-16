8bdaa53b9be345a6158ba37c4c0c48d7168ecc6a
michalwachowicz <wachowiczmichal1@gmail.com>
2024-08-16 15:34:31 +0200

    Fix accessability issues
    - Add title to iframe element
    - Add lang attribute to html element

--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,5 @@ import createContainer from "./layout/container";
 import createFooter from "./layout/footer";
 import { menu } from "./layout/menu";
 
+document.documentElement.lang = "en-US";
 document.body.append(menu, createContainer(), createFooter());
--- src/js/layout/contact.js
+++ src/js/layout/contact.js
@@ -40,6 +40,8 @@ export default function createContactSection() {
   const map = document.createElement("iframe");
   map.src =
     "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3022.183949017381!2d-73.98811752414316!3d40.757978734799835!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25855c6480299%3A0x55194ec5a1ae072e!2sTimes%20Square!5e0!3m2!1spl!2sit!4v1723810252855!5m2!1spl!2sit";
+  map.title =
+    "Google Maps showing the location of the Times Square in New York City";
   map.width = 480;
   map.height = 300;
   map.loading = "lazy";
